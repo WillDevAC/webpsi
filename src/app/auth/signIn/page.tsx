4e090940539d68d5aced0c46e40a37b499e94398
WILL SILVA <59093218+WillDevAC@users.noreply.github.com>
2024-09-14 03:40:36 -0300

    fix error

--- src/app/auth/signIn/page.tsx
+++ src/app/auth/signIn/page.tsx
@@ -1,42 +1,45 @@
-"use client";
-
+import React, { Suspense } from 'react';
 import { Card, Grid } from "@mui/material";
 import { LoginForm } from "@/components/forms/login-form";
 import { FeatureSection } from "@/components/feature-section";
 
-export default function AuthPage() {
-  return (
-    <>
-      <div className="flex justify-center items-center bg-gradient-to-r from-[#a023da] via-[#a023da] to-[#6c1b92] w-screen h-screen">
-        <Card
+const AuthPageContent = () => (
+  <div className="flex justify-center items-center bg-gradient-to-r from-[#a023da] via-[#a023da] to-[#6c1b92] w-screen h-screen">
+    <Card
+      sx={{
+        maxWidth: 1300,
+        width: "100%",
+        display: "flex",
+        overflow: "hidden",
+        boxShadow: "none",
+        backgroundColor: "transparent",
+      }}
+    >
+      <Grid container spacing={3}>
+        <Grid
+          item
+          xs={0}
+          md={6}
           sx={{
-            maxWidth: 1300,
-            width: "100%",
-            display: "flex",
-            overflow: "hidden",
-            boxShadow: "none",
-            backgroundColor: "transparent",
+            display: { xs: "none", md: "flex" },
+            flexDirection: "column",
+            justifyContent: "center",
           }}
         >
-          <Grid container spacing={3}>
-            <Grid
-              item
-              xs={0}
-              md={6}
-              sx={{
-                display: { xs: "none", md: "flex" },
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-            >
-              <FeatureSection />
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <LoginForm />
-            </Grid>
-          </Grid>
-        </Card>
-      </div>
-    </>
+          <FeatureSection />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <LoginForm />
+        </Grid>
+      </Grid>
+    </Card>
+  </div>
+);
+
+export default function AuthPage() {
+  return (
+    <Suspense fallback={<div>Aguarde...</div>}>
+      <AuthPageContent />
+    </Suspense>
   );
 }
